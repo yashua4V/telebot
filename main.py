94689b4b354c9b1da0aa9ad12b5342691276b1cc
root <903645896@qq.com>
2020-03-12 20:28:45 +0800

    up

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 
 import  telebot
+from telebot import types
 from config import TOKEN , WXIP
 import requests
 import os,base64
 from PIL import Image
 from io import BytesIO
+import random
 import json
 
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['linkstart'])
 def link_open(message):
-    if message.from.username == 'linke9054592735':
+    print(str(message))
+    if message.from_user.username == 'linke9054592735':
         bot.send_message(message.chat.id, 'master')
+        os.system('ssh -p 34567 root@%s "/root/wxvpn.sh 1800" & ' % WXIP)
+        bot.send_message(message.chat.id, 'vpn已开，时限30分钟，vpn信息请咨询管理')
+    elif message.from_user.username == 'rouli666':
+        
+        os.system('ssh -p 34567 root@%s "/root/wxvpn.sh 1800" & ' % WXIP)
+        bot.send_message(message.chat.id, 'vpn已开，时限30分钟，vpn信息请咨询管理')
     else:
         bot.send_message(message.chat.id, '你没有权限')
     pass
@@ -23,6 +32,35 @@ def send_welcome(message):
 @bot.message_handler(commands=['help'])
 def send_welcome1(message):
     bot.send_message(message.chat.id, text='点餐请发/+菜名或/菜名,如‘/馒头’不会搜索到烤馒头')
+@bot.message_handler(regexp='^/\*.*')
+def test(message):
+    myurl = 'http://127.0.0.1/getmenu'
+    chatid = message.chat.id
+    
+#   print ('\n'.join(['%s:%s' % item for item in types.__dict__.items()]))
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    print(str(markup.__dict__.items()))
+    itembtn1 = types.KeyboardButton('a')
+    itembtn2 = types.KeyboardButton('v')
+    itembtn3 = types.KeyboardButton('d')
+    print(str(itembtn1.__dict__.items()))
+    markup.add(itembtn1, itembtn2, itembtn3)
+    
+    if message.content_type == 'text':
+        text = message.text
+        data = {'tag':text}
+        re = requests.post(myurl,data)
+        r = json.loads(re.text)
+        if r['status'] == 'success':
+#            for i in r['img_list']:
+#                imgdata=base64.b64decode(i)
+#                bot.send_photo(chatid, imgdata)
+            imgdata = base64.b64decode(r['img_list'][random.randint(0,len(r['img_list'])-1)])
+            bot.send_photo(chatid, imgdata, reply_markup=markup)
+        else:
+            text = '没有搜索到菜单，发送菜单图片添加'
+            bot.send_message(chat_id=message.chat.id,reply_to_message_id=message.message_id,text=text)
+
 
 @bot.message_handler(regexp='^/\+.*')
 def command_url(message):
@@ -34,9 +72,11 @@ def command_url(message):
         re = requests.post(myurl,data)
         r = json.loads(re.text)
         if r['status'] == 'success':
-            for i in r['img_list']:
-                imgdata=base64.b64decode(i)
-                bot.send_photo(chatid, imgdata)
+#            for i in r['img_list']:
+#                imgdata=base64.b64decode(i)
+#                bot.send_photo(chatid, imgdata)
+            imgdata = base64.b64decode(r['img_list'][random.randint(0,len(r['img_list'])-1)])
+            bot.send_photo(chatid, imgdata)
         else:
             text = '没有搜索到菜单，发送菜单图片添加'
             bot.send_message(chat_id=message.chat.id,reply_to_message_id=message.message_id,text=text)
@@ -45,15 +85,18 @@ def command_url(message):
 def command_url_pls(message):
     myurl = 'http://127.0.0.1/getmenupls'
     chatid = message.chat.id
+    print(str(message))
     if message.content_type == 'text':
         text = message.text
         data = {'tag':text}
         re = requests.post(myurl,data)
         r = json.loads(re.text)
         if r['status'] == 'success':
-            for i in r['img_list']:
-                imgdata=base64.b64decode(i)
-                bot.send_photo(chatid, imgdata)
+#            for i in r['img_list']:
+#                imgdata=base64.b64decode(i)
+#                bot.send_photo(chatid, imgdata)
+            imgdata = base64.b64decode(r['img_list'][random.randint(0,len(r['img_list'])-1)])
+            bot.send_photo(chatid, imgdata)
         else:
             text = '没有搜索到菜单，发送菜单图片添加'
             bot.send_message(chat_id=message.chat.id,reply_to_message_id=message.message_id,text=text)
